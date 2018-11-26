ef6214f304d3fb3f8417cd84948293641b9ce4af
bodart <jerome.bodart@hei.yncrea.fr>
2018-11-26 16:59:43 +0100

    addinheritenceandmodelvalidations

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,3 +1,6 @@
 class Board < ApplicationRecord
   belongs_to :patient
+  validates :name, presence: true
+  has_many :board_sources
+  has_many :sources, through: :board_sources
 end
--- app/models/divergence.rb
+++ app/models/divergence.rb
@@ -1,3 +1,7 @@
 class Divergence < ApplicationRecord
   belongs_to :source
+
+  validates :position, presence: true
+  validates :character, presence: true
+  validates :position, presence: true, uniqueness: { scope: :source }
 end
