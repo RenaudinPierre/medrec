d79abd4916092b2dec3f985fdbe060f58b8deb0b
Hugo Joly <yh.joly@gmail.com>
2018-11-27 17:39:35 +0100

    Add modal for Patient Form

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,6 @@ import initNavbar from "../components/navbar";
 import AddSource from  "./card-source";
 import AddMedicament from  "./card-medicament";
 
+initNavbar();
 AddSource();
 AddMedicament();
-initNavbar();
