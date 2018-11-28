aa9ef0b6b11dd6e4bd13802d90cfbfb8718b89b3
Hugo Joly <yh.joly@gmail.com>
2018-11-28 17:06:20 +0100

    Starting to find a way for a datepicker in a modal

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,9 +2,12 @@ import "bootstrap";
 import initNavbar from "../components/navbar";
 import AddSource from  "./card-source";
 import AddMedicament from  "./card-medicament";
+// import calendarpickr from "../components/calendar";
+
 
 initNavbar();
 AddSource();
 AddMedicament();
+// calendarpickr();
 
 import "./drag-and-drop.js";
