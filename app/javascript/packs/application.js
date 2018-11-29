2db838396c1fc78986aa74811503e45e4f0df0bf
bodart <jerome.bodart@hei.yncrea.fr>
2018-11-29 16:49:03 +0100

    Did moving evolutive matrix for dragula

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,11 +1,12 @@
 import "bootstrap";
 import initNavbar from "../components/navbar";
+import "dragula/dist/dragula.css";
+import "./card-dragula.js";
 import InitSource from  "./card-source";
 import InitMedicament from  "./card-medicament"
 import InitDragula from "./card-dragula";
 
 initNavbar();
-InitSource(InitDragula);
 InitMedicament(InitDragula);
+InitSource(InitMedicament,InitDragula);
 
-import "./card-dragula.js";
--- app/javascript/packs/card-source.js
+++ app/javascript/packs/card-source.js
@@ -1,33 +1,35 @@
-function InitSource(callback) {
+function InitSource(callbackMed, callbackDragula) {
   const btn_source = document.querySelector('.btn-add-source');
 
   if (btn_source) {
     btn_source.addEventListener('click', function(e) {
       e.preventDefault();
-
-      let card_sources = document.querySelectorAll('.card-source');
-      const number = card_sources.length;
-      const max_card_in_source = card_sources[0].querySelectorAll('.card-invisible').length;
-      console.log(`${max_card_in_source}`);
-      const element_source = `<div class="card-source"><div class="card-source-invisible"></div><a class="btn-add-medicament" href="">+</a></div>`; //${number + 1}</div>`;
-      card_sources[card_sources.length-1].insertAdjacentHTML("afterend", element_source);
-      card_sources = document.querySelectorAll('.card-source');
-      const last_card = card_sources[card_sources.length-1]
-      last_card.setAttribute("id", `source_${number + 1}`);
-      let pas =0;
-      for (pas = 0; pas < max_card_in_source ; pas++) {
-        let element = `<div class="card-invisible" id="source_${number + 1}_card_${pas + 1}"></div>`;
-        last_card.querySelector('.card-source-invisible').insertAdjacentHTML("beforeend", element);
-      }
+      AddSource(btn_source);
       console.log("je suis ici");
-      console.log(callback);
 
-      callback();
+      callbackMed(callbackDragula);
     });
   }
 
 }
 
+function AddSource(btn_source) {
+  let card_sources = document.querySelectorAll('.card-source');
+  const number = card_sources.length;
+  const max_card_in_source = card_sources[0].querySelectorAll('.card-invisible').length;
+  console.log(`${max_card_in_source}`);
+  const element_source = `<div class="card-source"><div class="card-source-invisible"></div><a class="btn-add-medicament" id="btn_source_${number + 1}" href="">+</a></div>`; //${number + 1}</div>`;
+  card_sources[card_sources.length-1].insertAdjacentHTML("afterend", element_source);
+  // Add eventlistener to the new set of buttons
+  card_sources = document.querySelectorAll('.card-source');
+  const last_card = card_sources[card_sources.length-1]
+  last_card.setAttribute("id", `source_${number + 1}`);
+  let pas =0;
+  for (pas = 0; pas < max_card_in_source ; pas++) {
+    let element = `<div class="card-invisible" id="source_${number + 1}_card_${pas + 1}"></div>`;
+    last_card.querySelector('.card-source-invisible').insertAdjacentHTML("beforeend", element);
+  }
+}
 
 
 export default InitSource;
