d42a338396a175bc078a8b7d9428ed183877f3e3
Hugo Joly <yh.joly@gmail.com>
2018-12-06 12:33:08 +0100

    problem solved

--- app/models/divergence.rb
+++ app/models/divergence.rb
@@ -2,7 +2,6 @@ class Divergence < ApplicationRecord
   belongs_to :source
 
   validates :position, presence: true
-  validates :character, presence: true
   validates :position, presence: true, uniqueness: { scope: :source }
 
 
